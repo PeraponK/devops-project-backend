6e5efddc41f1ded896d03b1a683150472dc308ca
peraponk <pperapon11888@gmail.com>
2025-06-27 17:41:47 +0700

    remove ternary

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+// const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
